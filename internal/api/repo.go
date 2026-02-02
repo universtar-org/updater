78196a4c12b2b5e886bb5b1b056059519bc317a3
Fovir <fovir@disroot.org>
2026-02-02 23:50:59 +0800

    feat: handle status code

--- internal/api/repo.go
+++ internal/api/repo.go
@@ -15,16 +15,17 @@ type Repo struct {
 }
 
 // GetRepo Get repo information including description, number of stars, etc., via GitHub API.
-func (c *Client) GetRepo(ctx context.Context, owner, repo string) (*Repo, error) {
+func (c *Client) GetRepo(ctx context.Context, owner, repo string) (*Repo, int, error) {
 	req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/repos/%s/%s", owner, repo))
 	if err != nil {
-		return nil, err
+		return nil, http.StatusBadRequest, err
 	}
 
 	var r Repo
-	if err := c.do(req, &r); err != nil {
-		return nil, err
+	status, err := c.do(req, &r)
+	if err != nil {
+		return nil, status, err
 	}
 
-	return &r, nil
+	return &r, status, nil
 }
