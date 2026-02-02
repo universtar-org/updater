215d6784c5126c08482a41625c6f1234928769e6
Fovir <fovir@disroot.org>
2026-02-02 21:22:55 +0800

    refactor: move `GetDataFiles()` function into `reader.go`

--- internal/io/reader.go
+++ internal/io/reader.go
@@ -3,7 +3,9 @@ package io
 import (
 	"github.com/goccy/go-yaml"
 	"github.com/universtar-org/updater/internal/model"
+
 	"os"
+	"path/filepath"
 )
 
 func ReadYaml(path string) ([]model.Project, error) {
@@ -19,4 +21,20 @@ func ReadYaml(path string) ([]model.Project, error) {
 	}
 
 	return projects, nil
+}
+
+func GetDataFiles(dir string) ([]string, error) {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var paths []string
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			paths = append(paths, filepath.Join(dir, entry.Name()))
+		}
+	}
+
+	return paths, nil
 }
