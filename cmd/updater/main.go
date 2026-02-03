e8aea746a32fd8379697d60074d48c5b3e8b6b0d
Fovir <fovir@disroot.org>
2026-02-03 14:45:14 +0800

    refactor: move `update` function into `cmd/updater/main.go`

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/universtar-org/tools/internal/api"
 	"github.com/universtar-org/tools/internal/io"
-	"github.com/universtar-org/tools/internal/updater"
+	"github.com/universtar-org/tools/internal/utils"
 )
 
 func main() {
@@ -24,10 +25,47 @@ func main() {
 
 	for _, path := range list {
 		fmt.Println("Processing: ", path)
-		if err := updater.Update(client, ctx, path); err != nil {
+		if err := update(client, ctx, path); err != nil {
 			panic(err)
 		}
 	}
 
 	fmt.Println("Finished!")
+}
+
+// Update a file/user.
+func update(client *api.Client, ctx context.Context, path string) error {
+	const MAX_TAG_NUMBER = 5
+	owner := utils.ParseOwner(path)
+
+	projects, err := io.ReadYaml(path)
+	if err != nil {
+		return err
+	}
+
+	for i := range projects {
+		fmt.Printf("Processing: %v/%v\n", owner, projects[i].Repo)
+
+		repo, status, err := client.GetRepo(ctx, owner, projects[i].Repo)
+		if err != nil {
+			return err
+		}
+
+		if status != http.StatusOK {
+			return fmt.Errorf("Request Failed: Get %v", status)
+		}
+
+		tag_list := append([]string{repo.Language}, repo.Tags...)
+		if len(tag_list) > MAX_TAG_NUMBER {
+			tag_list = tag_list[:MAX_TAG_NUMBER]
+		}
+		projects[i].Description = repo.Description
+		projects[i].Stars = repo.Stars
+		projects[i].UpdatedAt = repo.UpdatedAt
+		projects[i].Tags = tag_list
+	}
+
+	io.WriteYaml(projects, path)
+
+	return nil
 }
